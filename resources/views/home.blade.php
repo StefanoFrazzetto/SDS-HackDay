5308fc6fba4c9f4fc99cb1811e93c0cf189027e4
Cammmmy <cammy219@live.co.uk>
2016-11-19 11:59:03 +0000

    Map
    Added map to index page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<style>
+		html, body {
+			height: 100%;
+			margin: 0;
+			padding: 0;
+		}
+		#map {
+			height: 100%;
+		}
+	</style>
     <title>Debuggers - Jobs concentration map</title>
 </head>
 <body>
-
-    <h1></h1>
-
+<div id="map"></div>
+		<script src="{{ url('js/map.js') }}"></script>
+		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDICGxukonu4A08SP3IrAlM0madI_e1QBY&callback=initMap"></script>
 </body>
 </html>
