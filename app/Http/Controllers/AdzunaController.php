8b71adf5fa5ff870a1dcfbe756679451f7dd9727
Stefano Frazzetto <s.frazzetto22@gmail.com>
2016-11-19 12:20:27 +0000

    Added Adzuna controller and API key

---
+++ app/Http/Controllers/AdzunaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class GetDataController extends Controller
+{
+    protected $adzuna_id;
+    protected $adzuna_key;
+
+    public function __construct()
+    {
+        $this->adzuna_id = getenv("ADZUNA_ID");
+        $this->adzuna_key = getenv("ADZUNA_KEY");
+    }
+
+    public function get($key) {
+        $url = urlencode("http://api.adzuna.com/v1/api/jobs/gb/categories?app_id=$this->adzuna_id&app_key=$this->adzuna_key&&content-type=application/json");
+        $json = json_decode(file_get_contents($url), true);
+        return response()->json(['data' => $json]);
+    }
+}
